738efb59bdeab7eab9f9403dea5e2acbd510519d
Jeeva.R <123143909+jeeva1511@users.noreply.github.com>
2024-12-26 23:24:05 +0530

    Update ToDoHome.jsx

--- src/components/ToDoHome.jsx
+++ src/components/ToDoHome.jsx
@@ -22,7 +22,7 @@ const ToDoHome = () => {
   const [editDescription, setEditDescription] = useState("");
 
   // BackEnd API
-  const Url = "https://todoapp-backend-22nb.onrender.com";
+  const Url = "https://todoapp-backend-22nb.onrender.com/todo";
 
   // Post method
   const handleSubmit = async () => {
